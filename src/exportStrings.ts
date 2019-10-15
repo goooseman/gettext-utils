603ce06642d181c0b2bedcf501782a4245b6d100
Alex Zinkevych <alexzinkevych@yandex.ua>
2019-10-15 16:30:48 +0300

    fix: 🐛  Removed column and line numbers from pot files

--- src/exportStrings.ts
+++ src/exportStrings.ts
@@ -2,10 +2,12 @@ import { existsSync, mkdirp, writeFile } from "fs-extra";
 import { po } from "gettext-parser";
 import * as path from "path";
 import { extractMessagesFromGlob, toPot } from "react-gettext-parser";
+
 import lionessConfig from "./config/lioness.config";
 import { getPoParsed } from "./importStrings";
 import updateTranslations from "./updateTranslations";
 import arePotsDifferent from "./utils/arePotsDifferent";
+import { optimizeMessageForGit } from "./utils/optimizeForGit";
 import { getPackageNameAndVersion } from "./utils/packageInfo";
 
 const exportStrings = async (
@@ -38,7 +40,11 @@ const getUpdatedTemplateContents = async (
   templateFilePath: string,
 ): Promise<string | undefined> => {
   const packageName = await getPackageNameAndVersion();
-  const newMessages = extractMessagesFromGlob([inputFilesGlob], lionessConfig);
+  const newMessages = extractMessagesFromGlob(
+    [inputFilesGlob],
+    lionessConfig,
+  ).map(optimizeMessageForGit);
+
   const newPot = toPot(newMessages, {
     transformHeaders: packageName
       ? (x) => ({
